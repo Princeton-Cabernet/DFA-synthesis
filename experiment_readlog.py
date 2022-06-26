a852ce6251968aabd3ace2bad5ac2538b828f3a9
Xiaoqi Chen <chenxiaoqino@gmail.com>
2022-06-26 14:02:16 -0400

    Add additional exp configs

--- experiment_readlog.py
+++ experiment_readlog.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     files = ["zoom_simple.json", "mobiledevice.json", "tcp_open.json", "simple.json"] \
             + ["fingerprint_%s.json" % suf for suf in ["16x1", "8x3", "10x3", "12x3", "8x4", "10x4", "12x4"] ] \
             + ["s%d.json" % i for i in range(2,5)] + ["p%d.json" % i for i in range(2,5)]
-    params = ["SingleRegAct","FourRegAct","TwoCond","TwoSlot","FourBranch"]
+    params = ["SingleRegAct","FourRegAct","TwoCond","TwoSlot","FourBranch","TwoCondSplitNode","TwoSplitSplitNode","FourBranchSplitNode"]
 
     print('input\t'+'\t'.join(params))
     for f in files:
