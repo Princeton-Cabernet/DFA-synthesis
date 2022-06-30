a508b62bb421b69394889c3332dc4f39f1c6adbc
Xiaoqi Chen <xiaoqic@cs.princeton.edu>
2022-06-30 12:43:56 -0400

    add script generator

---
+++ experiment_100fingerprint/scripts/gen.py
@@ -0,0 +1,4 @@
+N=20
+for a in 'Assignment3,Arithmetic3,TwoCond3,TwoSlot3,FourBranch3'.split(','):
+    for l in '1x8,1x10,1x12,1x14,1x16,1x18,2x4,2x6,2x8,2x10,3x4,3x6,3x8,4x4,4x6'.split(','):
+        print(f"python3 ../../experiment_100fingerprint_onetask.py  {l} {a} tryWithBools2.py  -N {N} > {l}.{a}.sh")
